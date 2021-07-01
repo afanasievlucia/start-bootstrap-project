e95b4c43eaaf4ae3d8a9e2896c9d6a7cdbdf7783
Afanasiev <afanasiev.lucia@gmail.com>
2021-07-01 23:24:11 +0300

    change post with props, with global declaration

--- src/App.js
+++ src/App.js
@@ -2,13 +2,41 @@ import NavBar from './home-components/Navigation-bar';
 import Header from './home-components/Header';
 import Main from './home-components/Main';
 import Footer from './home-components/Footer';
+import { useState } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: 'Man must explore, and this is exploration at its greatest',
+      subtitle: 'Problems look mighty small from 150 miles up',
+      dayPost: 'September 24, 2021'
+    },
+    {
+      id: 2,
+      title: "I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.",
+      subtitle: '',
+      dayPost: 'September 18, 2021'
+    },
+    {
+      id: 3,
+      title: "Science has not yet mastered prophecy",
+      subtitle: 'We predict too much for the next year and yet far too little for the next ten.',
+      dayPost: 'August 24, 2021'
+    },
+    {
+      id: 1,
+      title: 'Failure is not an option',
+      subtitle: 'Many say exploration is part of our destiny, but it’s actually our duty to future generations.',
+      dayPost: 'July 8, 2021'
+    }
+  ])
+
   return (
     <>
     <NavBar />
     <Header />
-    <Main />
+    <Main posts={posts}/>
     <Footer />
     </>
   );
--- src/home-components/Main.js
+++ src/home-components/Main.js
@@ -1,32 +1,19 @@
-import FirstPost from "./First-post"
-import SecondPost from "./Second-post"
-import ThirdPost from "./Third-post"
-import FourthPost from "./Fourth-post"
+import Post from "./Post"
 
-const Main = () => {
-    return (
-        <div className="container px-4 px-lg-5">
-            <div className="row gx-4 gx-lg-5 justify-content-center">
-                <div className="col-md-10 col-lg-8 col-xl-7">
-                  <FirstPost />
-                    {/* Divider */}
-                    <hr className="my-4" /><hr className="my-4" />
-                  <SecondPost />  
-                    {/* Divider */}
-                    <hr className="my-4" /><hr className="my-4" />
-                  <ThirdPost />  
-                    {/* Divider */}
-                    <hr className="my-4" /><hr className="my-4" />
-                    {/*<!-- Post preview-->*/}
-                   <FourthPost /> 
-                    {/*<!-- Divider-->*/}
-                    <hr className="my-4" /><hr className="my-4" />
-                   {/*<!-- Pager-->*/}
-                    <div className="d-flex justify-content-end mb-4"><a className="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div>
-                </div>
-            </div>
+const Main = ({posts}) => {
+  return (
+    <div className="container px-4 px-lg-5">
+      <div className="row gx-4 gx-lg-5 justify-content-center">
+        <div className="col-md-10 col-lg-8 col-xl-7">
+          {posts.map((post) => (
+            <Post post={post}/>
+          ))}
+          <div className="d-flex justify-content-end mb-4"><a className="btn btn-primary text-uppercase" href="#!">Older Posts →</a>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Main
